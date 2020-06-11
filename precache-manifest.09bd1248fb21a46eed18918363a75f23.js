c58d9671c38b6bda21de4f5dca12ca8bd63ad406
Gustaf von Grothusen <gustaf.grothusen@gmail.com>
2020-06-11 15:35:21 +0200

    Updates

---
+++ precache-manifest.09bd1248fb21a46eed18918363a75f23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f768b0dc2f5515feaf3dfbc4ff63ffb8",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e8042c0d22702b6d7cf0",
+    "url": "/monsters/static/css/main.78206b23.chunk.css"
+  },
+  {
+    "revision": "93371b6728db4e11b826",
+    "url": "/monsters/static/js/2.107ed972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.107ed972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8042c0d22702b6d7cf0",
+    "url": "/monsters/static/js/main.b7fbdac1.chunk.js"
+  },
+  {
+    "revision": "a297aad1fbf6f62d6424",
+    "url": "/monsters/static/js/runtime-main.b8ed5776.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
